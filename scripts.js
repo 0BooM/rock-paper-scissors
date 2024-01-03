7ac093222ee7e64679f123ba9f23e3b5a5df544d
0BooM <149960425+0BooM@users.noreply.github.com>
2024-01-03 15:04:33 +0100

    Create function getComputerSelection
    Created a function which gets random computerSelection (rock/paper/scissors)

--- scripts.js
+++ scripts.js
@@ -1,1 +1,32 @@
-console.log("hello wrold")
+let computerSelection;
+
+function getComputerSelection(){
+    let randomNumber = Math.floor((Math.random() * 3) + 1);
+    console.log(randomNumber);
+
+    switch(randomNumber){
+        case 1:
+            computerSelection = "rock"
+            break;
+        case 2:
+            computerSelection = "paper"
+            break;
+        case 3:
+            computerSelection = "scissors";
+            break;
+    };
+    console.log(computerSelection);
+}
+getComputerSelection();
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = prompt("What do you pick?", "rock | paper | scissors").toLowerCase();
+    console.log(playerSelection);
+
+    switch(playerSelection, computerSelection){
+        case playerSelection === "rock" && computerSelection === "paper":
+            console.log("You lose! Paper beats Rock");
+    }
+};
+
+playRound();
