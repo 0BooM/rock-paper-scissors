a636a1952a46c6e2b981e201668a592b04ef1eff
0BooM <149960425+0BooM@users.noreply.github.com>
2024-01-03 16:05:17 +0100

    Change console.log in getComputerSelection() to return

--- scripts.js
+++ scripts.js
@@ -20,17 +20,17 @@ function playRound(playerSelection, computerSelection){
     if(playerSelection === "paper" && computerSelection === "rock"
     || playerSelection === "scissors" && computerSelection === "paper" 
     || playerSelection === "rock" && computerSelection === "scissors"){
-        console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
+        return `You win! ${playerSelection} beats ${computerSelection}.`;
     }
 
     else if(playerSelection === "rock" && computerSelection ==="paper" 
     || playerSelection === "scissors" && computerSelection === "rock"
     || playerSelection === "paper" && computerSelection === "scissors"){
-        console.log(`You lose ${computerSelection} beats ${playerSelection}.`);
+        return `You lose ${computerSelection} beats ${playerSelection}.`;
     }
 
     else{
-        console.log(`It's a tie!`);
+        return `It's a tie!`;
     }
 };
 
@@ -40,4 +40,4 @@ console.log(`Computer chose: ${computerSelection}`);
 let playerSelection = prompt("What do you pick?", "rock | paper | scissors").toLowerCase();
 console.log(`Player chose: ${playerSelection}`)
 
-playRound(playerSelection, computerSelection);
+console.log(playRound(playerSelection, computerSelection));
